d8dea11fd2cf3752667c814a1dedef13c01157ac
spencer-weaver <sweavertheslayr@outlook.com>
2025-04-05 19:12:18 -0400

    some functional upgrades

--- lua/sweaver/init.lua
+++ lua/sweaver/init.lua
@@ -1,4 +1,19 @@
 require("sweaver.remap")
 require("sweaver.packer")
-print("hello from sweaver")
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- whitespace
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = '▸ ',
+	trail = '·',
+	extends = '⟩',
+	precedes = '⟨'
+}
 
